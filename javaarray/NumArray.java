8469dab3a836c8a6427edde8204a0bf0cb2dbcf3
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-05-20 13:27:22 +0530

    week 2

---
+++ javaarray/NumArray.java
@@ -0,0 +1,11 @@
+public class NumArray {
+		public static void main(String[] args) {
+			int[] number = new int[10];
+			for( int i = 0; i < number.length; i++) {
+				number[i] = i+1;
+			}
+			for( int i = 0; i < number.length; i++) {
+				System.out.println("index" +" " + i +" = " + number[i]);
+			}
+		}
+	}
---
+++ javaarray/ReadAndStoreNames.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReadAndStoreNames {
+
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("How many names would you like to enter?");
+    int n = scan.nextInt(); //Ensures you take an integer
+    System.out.println("Enter the " + n + " names: ");
+
+    String[] names = new String[n];
+    //store the names in an array
+    for (int i = 0; i <= names.length; i++){
+        names[i] = scan.nextLine();
+        }
+    //sequentially print the names and upperCase them
+    for (int i = 0; i <= names.length; i++){
+        System.out.println(names[i].toUpperCase());
+        }
+
+}
+
+}
