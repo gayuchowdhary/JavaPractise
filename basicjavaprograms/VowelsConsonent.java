045ac0c72f8a3df8c8fd57889ad9d87b4e88ca9a
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-05-30 12:45:37 +0530

    vowels

---
+++ basicjavaprograms/VowelsConsonent.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VowelsConsonent {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // char ch = (char)scan.nextInt();
+        char ch = scan.next().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+        {
+            System.out.println(ch + " is vowel");
+        }
+        else
+            System.out.println(ch + " is consonant");
+    }
+}
